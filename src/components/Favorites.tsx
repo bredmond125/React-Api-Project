5a534b5b65a46d59325c0869d57bcc315df395a4
Greg <gregorytlivingston@gmail.com>
2021-08-02 14:53:41 -0400

    Took out code context being wierd

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
+import { Props } from "./RecipeCard";
 
 
-function Favorites() {
+
+
+
+
+
+function Favorites({label, image, url}: Props) {
+
+    
+
+    
+    // function handleAdd(newItem: Props) {
+  
+    //     setFavs({
+    //         ...favs,
+    //         newItem
+    //     })
+
+    //     addToFav({
+    //         label,
+    //         image,
+    //         url
+    //     });
+    // }
+
+    
+
 
     return (
         <div className="Favorites">
+            <div className="card-container">
+                <div className="image-container">
+                    <img src={image} alt="Food"/>
+                </div>
+                <div className="label-container">
+                    <p className="card-label">
+                        {label}
+                    </p>
+                </div>
+                <div className="link-container">
+                    <a href={url} target="_blank">Original Recipe</a>  
+                </div>
+                <div>
+                   
+                </div>
+            </div>
 
         </div>
     )
