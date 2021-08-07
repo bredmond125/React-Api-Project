ca8f5eb2faa07cf7b2b72591985bdfe222aa1a22
Greg <gregorytlivingston@gmail.com>
2021-08-07 11:56:18 -0400

    adding index card and detz

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,3 +1,4 @@
+import "../styles/Detail.css";
 import React, { useContext } from 'react'
 import { Recipe } from '../models/Item'
 import {useLocation} from 'react-router-dom'
@@ -15,11 +16,63 @@ function Description() {
     // const recipe = location.recipe
     
     const newDetails = details;
+
+    let healthInfo = newDetails.recipe.healthLabels;
+
+   
+
+    let shortHeath = [];
+    for(let i = 0; i <= 7; i++){
+        shortHeath.push(healthInfo[i]);
+
+    }
+    let healthDetail = shortHeath.join(", ")
+
+    let dishType = newDetails.recipe.dishType;
+
+    let dietLabels = newDetails.recipe.dietLabels.join(", ")
+
+
+    let cals = newDetails.recipe.calories;
+
+    let finalCal = Math.round(cals);
+
+    let img = newDetails.recipe.image;
+
+    
+
+
+
+    
     return (
         <div className="Description">
-            {newDetails.recipe.label}
-            {newDetails.recipe.image}
-            {newDetails.recipe.ingredientLines}
+            <div className="ImageSection">
+                <img src={img} alt="recipe final" />
+            </div>
+            <div className="IndexCard">
+                <div className="Title">
+                    {newDetails.recipe.label}
+                </div>
+                
+                <div className="LineOne">
+                    <p>Dish Type: {dishType} </p>
+                </div>
+                <div className="LineTwo">
+                    <p>Diet Lables: {dietLabels} </p>
+                </div>
+                <div className="LineThree">
+                    <p>Calories: {finalCal} </p>
+                </div>
+                <div className="LineFour">
+                    {healthDetail}
+                </div>
+                <div className="LineFive">
+                    {newDetails.recipe.ingredientLines}
+                </div>
+
+                
+            </div>
+            
         </div>
     )
 }
--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,10 +1,12 @@
 export interface Recipe {
     recipe: {
-    label: string;
-    url: string;
-    calories?: number;
-    image?: string;
-    heathLabels?: string[];
-} 
+        label: string;
+        url: string;
+        calories?: number;
+        image?: string;
+        heathLabels?: string[];
+        dishType?: string[];
+        dietLabels?: string[];
+    } 
 }
 
